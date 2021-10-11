2b0d3252b9aca14b7f6c7a71d3dee628cb48e191
Ronen302 <ronen302@gmail.com>
2021-10-11 10:15:14 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tuple = GetNamedValues(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+            Console.WriteLine(tuple.count);
+            Console.WriteLine(tuple.sum);
+            Console.WriteLine(tuple.proisv);
+
+            Console.Read();
+        }
+        private static (int sum, int count, int proisv) GetNamedValues(int[] numbers)
+        {
+            var result = (sum: 0, count: 0, proisv: 1);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                result.sum += numbers[i];
+                result.count++;
+                result.proisv *= numbers[i];
+
+            }
+            return result;
+        }
+    }
+}
